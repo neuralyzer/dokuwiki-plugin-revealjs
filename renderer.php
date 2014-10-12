32e8b4eab45b865ec4f02d937ccd29363569c6ea
neuralyzer <emmanuel.klinger@gmail.com>
2014-10-12 15:39:50 +0200

    center table

--- renderer.php
+++ renderer.php
@@ -220,7 +220,7 @@ class renderer_plugin_revealjs extends Doku_Renderer_xhtml {
         if($pos !== null) {
             $class .= ' '.$this->startSectionEdit($pos, 'table');
         }
-        $this->doc .= '<table>'.
+        $this->doc .= '<table class="centeredtable">'.
             DOKU_LF;
     }
 
