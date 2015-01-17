ae72dad1c7db98a97403f9b92210631c04b68021
neuralyzer <emmanuel.klinger@gmail.com>
2015-01-17 16:01:46 +0100

    test

--- syntax/background.php
+++ syntax/background.php
@@ -1,38 +1,170 @@
 <?php
 /**
- * Plugin Now: Inserts a timestamp.
+ * Plugin Skeleton: Displays "Hello World!"
+ *
+ * Syntax: <TEST> - will be replaced with "Hello World!"
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Christopher Smith <chris@jalakai.co.uk>
  */
 
-// must be run within DokuWiki
-if(!defined('DOKU_INC')) die();
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
 
 /**
  * All DokuWiki plugins to extend the parser/rendering mechanism
  * need to inherit from this class
  */
-class syntax_plugin_revealjs_now extends DokuWiki_Syntax_Plugin {
+class syntax_plugin_test extends DokuWiki_Syntax_Plugin {
 
-    public function getType() { return 'substition'; }
-    public function getSort() { return 32; }
 
-    public function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('\[NOW\]',$mode,'plugin_revealjs_now');
+
+    /**
+     * Get the type of syntax this plugin defines.
+     *
+     * @param none
+     * @return String <tt>'substition'</tt> (i.e. 'substitution').
+     * @public
+     * @static
+     */
+    function getType(){
+        return 'substition';
     }
 
-    public function handle($match, $state, $pos, Doku_Handler $handler) {
-        return array($match, $state, $pos);
+    /**
+     * What kind of syntax do we allow (optional)
+     */
+//    function getAllowedTypes() {
+//        return array();
+//    }
+
+    /**
+     * Define how this plugin is handled regarding paragraphs.
+     *
+     * <p>
+     * This method is important for correct XHTML nesting. It returns
+     * one of the following values:
+     * </p>
+     * <dl>
+     * <dt>normal</dt><dd>The plugin can be used inside paragraphs.</dd>
+     * <dt>block</dt><dd>Open paragraphs need to be closed before
+     * plugin output.</dd>
+     * <dt>stack</dt><dd>Special case: Plugin wraps other paragraphs.</dd>
+     * </dl>
+     * @param none
+     * @return String <tt>'block'</tt>.
+     * @public
+     * @static
+     */
+//    function getPType(){
+//        return 'normal';
+//    }
+
+    /**
+     * Where to sort in?
+     *
+     * @param none
+     * @return Integer <tt>6</tt>.
+     * @public
+     * @static
+     */
+    function getSort(){
+        return 999;
     }
 
-    public function render($mode, Doku_Renderer $renderer, $data) {
-        // $data is what the function handle return'ed.
+
+    /**
+     * Connect lookup pattern to lexer.
+     *
+     * @param $aMode String The desired rendermode.
+     * @return none
+     * @public
+     * @see render()
+     */
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('<TEST>',$mode,'plugin_test');
+//      $this->Lexer->addEntryPattern('<TEST>',$mode,'plugin_test');
+    }
+
+//    function postConnect() {
+//      $this->Lexer->addExitPattern('</TEST>','plugin_test');
+//    }
+
+
+    /**
+     * Handler to prepare matched data for the rendering process.
+     *
+     * <p>
+     * The <tt>$aState</tt> parameter gives the type of pattern
+     * which triggered the call to this method:
+     * </p>
+     * <dl>
+     * <dt>DOKU_LEXER_ENTER</dt>
+     * <dd>a pattern set by <tt>addEntryPattern()</tt></dd>
+     * <dt>DOKU_LEXER_MATCHED</dt>
+     * <dd>a pattern set by <tt>addPattern()</tt></dd>
+     * <dt>DOKU_LEXER_EXIT</dt>
+     * <dd> a pattern set by <tt>addExitPattern()</tt></dd>
+     * <dt>DOKU_LEXER_SPECIAL</dt>
+     * <dd>a pattern set by <tt>addSpecialPattern()</tt></dd>
+     * <dt>DOKU_LEXER_UNMATCHED</dt>
+     * <dd>ordinary text encountered within the plugin's syntax mode
+     * which doesn't match any pattern.</dd>
+     * </dl>
+     * @param $aMatch String The text matched by the patterns.
+     * @param $aState Integer The lexer state for the match.
+     * @param $aPos Integer The character position of the matched text.
+     * @param $aHandler Object Reference to the Doku_Handler object.
+     * @return Integer The current lexer state for the match.
+     * @public
+     * @see render()
+     * @static
+     */
+    function handle($match, $state, $pos, &$handler){
+        switch ($state) {
+            case DOKU_LEXER_ENTER :
+                break;
+            case DOKU_LEXER_MATCHED :
+                break;
+            case DOKU_LEXER_UNMATCHED :
+                break;
+            case DOKU_LEXER_EXIT :
+                break;
+            case DOKU_LEXER_SPECIAL :
+                break;
+        }
+        return array();
+    }
+
+    /**
+     * Handle the actual output creation.
+     *
+     * <p>
+     * The method checks for the given <tt>$aFormat</tt> and returns
+     * <tt>FALSE</tt> when a format isn't supported. <tt>$aRenderer</tt>
+     * contains a reference to the renderer object which is currently
+     * handling the rendering. The contents of <tt>$aData</tt> is the
+     * return value of the <tt>handle()</tt> method.
+     * </p>
+     * @param $aFormat String The output format to generate.
+     * @param $aRenderer Object A reference to the renderer object.
+     * @param $aData Array The data created by the <tt>handle()</tt>
+     * method.
+     * @return Boolean <tt>TRUE</tt> if rendered successfully, or
+     * <tt>FALSE</tt> otherwise.
+     * @public
+     * @see handle()
+     */
+    function render($mode, &$renderer, $data) {
         if($mode == 'xhtml'){
-            /** @var Doku_Renderer_xhtml $renderer */
-            $renderer->doc .= date('r');
+            $renderer->doc .= "Hello World!";            // ptype = 'normal'
+//            $renderer->doc .= "<p>Hello World!</p>";     // ptype = 'block'
             return true;
         }
         return false;
     }
 }
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
+?>
