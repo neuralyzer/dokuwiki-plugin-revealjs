db6c719b8aa4a2519e4c66b8f9684722db8da886
neuralyzer <emmanuel.klinger@gmail.com>
2015-01-19 18:49:35 +0100

    background image fix

--- renderer.php
+++ renderer.php
@@ -196,10 +196,10 @@ class renderer_plugin_revealjs extends Doku_Renderer_xhtml {
         }
         $this->doc .= '<h'.$level.'>';
         $this->doc .= $this->_xmlEntities($text);
-        $this->doc .= '</h'.$level.'>'.DOKU_LF;
         if (strlen($text) == 0) {
             $this->doc .='.'; //firefox pdf print hack
         }
+        $this->doc .= '</h'.$level.'>'.DOKU_LF;
     }
 
     /**
