45af38b2ca0cc04b81f8cfff9b043b626a555eeb
Ottmar Gobrecht <ogobrecht@users.noreply.github.com>
2016-10-03 16:51:47 +0200

    correct print PDF link for rewrite enabled wiki's

--- action.php
+++ action.php
@@ -22,37 +22,44 @@ class action_plugin_revealjs extends DokuWiki_Action_Plugin {
         }
     }
 
-    /* close last edit section correctly (missing </div>), because we close sections
-    only when a new one is opened - and this logic fails for the last section in the
-    document */
     function _renderer_content_postprocess(&$event, $param) {
-        $search = '<!-- EDIT';
-        $replace = '</div><!-- EDIT';
-        $pos = strrpos($event->data[1],$search);
-        // replacement only for wiki pages, not for presentation
         if ($_GET['do'] !== 'export_revealjs' &&
-            $this->getConf('revealjs_active_and_user_can_edit_and_show_slide_details') &&
-            $pos !== false) {
-            $event->data[1] = substr_replace($event->data[1], $replace, $pos, strlen($search));
-            //dbglog('Plugin revealjs - hook RENDERER_CONTENT_POSTPROCESS - closing last edit section (missing </div>).', __FILE__.' line '.__LINE__);
-        }
-        if ($this->getConf('slides_with_unknown_direction')) {
+            $this->getConf('revealjs_active_and_user_can_edit_and_show_slide_details')) {
+
+            /* close last edit section correctly (missing </div>), because we close sections
+            only when a new one is opened - and this logic fails for the last section in the
+            document */
+            $search = '<!-- EDIT';
+            $replace = '</div><!-- EDIT';
+            $pos = strrpos($event->data[1],$search);
+            if ($pos !== false) {
+                $event->data[1] = substr_replace($event->data[1], $replace, $pos, strlen($search));
+                //dbglog('Plugin revealjs - hook RENDERER_CONTENT_POSTPROCESS - closing last edit section (missing </div>).', __FILE__.' line '.__LINE__);
+            }
+
+            // correct link for PDF export and fixing unknown slide directions in wiki page with jQuery
             $event->data[1] .='
 <script>
 jQuery(document).ready(function(){
+    jQuery(".slide-export-link a:last").each(function(){
+        var elem = jQuery(this);
+        var count = (elem.attr("href").match(/\?/g) || []).length;
+        if (count == 0) {
+            elem.attr("href", elem.attr("href").replace("&print-pdf","?print-pdf"));
+        }
+    });'.($this->getConf('slides_with_unknown_direction')?'
     jQuery(".fix-my-direction").each(function(){
-        element = jQuery(this);
-        element.removeClass("fix-my-direction");
-        if (element.next().find("h1'.($this->getConf('horizontal_slide_level')==2?',h2':'').'").length > 0) {
-            element.text("→" + element.text());
+        var elem = jQuery(this);
+        elem.removeClass("fix-my-direction");
+        if (elem.next().find("h1'.($this->getConf('horizontal_slide_level')==2?',h2':'').'").length > 0) {
+            elem.text("→" + elem.text());
         }
         else {
-            element.text("↓" + element.text());
+            elem.text("↓" + elem.text());
         }
-    });
+    });':'').'
 });
 </script>';
-            //dbglog('Plugin revealjs - hook RENDERER_CONTENT_POSTPROCESS - fixing unknown slide direction in wiki page with jQuery.', __FILE__.' line '.__LINE__);
         }
     }
 
